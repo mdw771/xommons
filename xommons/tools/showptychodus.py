85c979f24d10576f688b06a02ce5f634893e9db5
Ming Du <ming.du101@gmail.com>
2026-04-10 15:54:39 -0500

    FEAT: extend showptychodus with new variable options for 'ft-probe' and 'dp'

--- xommons/tools/showptychodus.py
+++ xommons/tools/showptychodus.py
@@ -13,7 +13,7 @@ import xommons.plot as xp
 def main(argv=None):
     parser = argparse.ArgumentParser()
     parser.add_argument("fname")
-    parser.add_argument("var", choices=["object", "probe", "positions", "pixelsize"])
+    parser.add_argument("var", choices=["object", "probe", "positions", "pixelsize", "ft-probe", "dp"])
     args = parser.parse_args(argv)
 
     with h5py.File(args.fname, "r") as f:
@@ -30,9 +30,27 @@ def main(argv=None):
             fig = xp.plot_ptychi_positions(positions)
             plt.show()
             plt.close(fig)
-        else:
+        elif args.var == "pixelsize":
             pixel_height = f["object"].attrs["pixel_height_m"]
             pixel_width = f["object"].attrs["pixel_width_m"]
             print(f"Pixel height: {pixel_height} m, pixel width: {pixel_width} m")
+        elif args.var == "ft-probe":
+            probe = f["probe"][...]
+            fig, ax = plt.subplots(1, 2)
+            ax[0].imshow(np.abs(np.fft.fftshift(np.fft.fft2(probe[0, 0])) ** 2))
+            ax[1].imshow((np.abs(np.fft.fftshift(np.fft.fft2(probe[0]), axes=(1, 2)) ** 2)).sum(axis=0))
+            ax[0].set_title("First mode")
+            ax[1].set_title("All modes")
+            plt.show()
+            plt.close(fig)
+        elif args.var == "dp":
+            if "dp" not in f:
+                raise ValueError("Dataset 'dp' not found in file. Make sure your input file is a DP file.")
+            dp = f["dp"][0]
+            fig, ax = plt.subplots(1, 1)
+            im = ax.imshow(dp)
+            plt.colorbar(im)
+            plt.show()
+            plt.close(fig)
 
     return 0
